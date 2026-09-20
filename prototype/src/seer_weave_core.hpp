98bb995e77f277b6184ee69043163edfad6f8178
Sargon 17 Green <eliezer.green003@gmail.com>
2026-09-20 14:24:30 +0300

    WS06: expose internal mobile cancellation hook

--- prototype/src/seer_weave_core.hpp
+++ prototype/src/seer_weave_core.hpp
@@ -1,7 +1,9 @@
 #pragma once
 #include "sauce_fast127_v12.hpp"
 #include <vector>
+#include <atomic>
 namespace seer_native::detail {
+void set_thread_cancel_flag(const std::atomic_bool* flag);
 std::vector<int> weave_month_prefix(
     const std::vector<int>& monthLengths,
     const FSauce& structSauce,
